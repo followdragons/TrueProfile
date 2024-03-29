c0ad3572b2a6613d85fc1822fea547b3cd2a5bfd
broiler99 <wer@cardify.cc>
2024-03-29 10:56:21 +0100

    ini wallet connector

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import twaLogo from './assets/tapps.png'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-import WebApp from '@twa-dev/sdk'
+import {TonConnectButton, TonConnectUIProvider} from "@tonconnect/ui-react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://ton.org/dev" target="_blank">
-          <img src={twaLogo} className="logo" alt="TWA logo" />
-        </a>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>TWA + Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      {/*  */}
-      <div className="card">
-        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
-            Show Alert
-        </button>
-      </div>
-    </>
+      <TonConnectUIProvider
+          manifestUrl="https://valeryv99.github.io/TonRockPaperScissors/tonconnect-manifest.json"
+          actionsConfiguration={{
+            twaReturnUrl: 'https://t.me/<YOUR_APP_NAME>'
+        }}
+      >
+          <span>My App with React UI</span>
+          <TonConnectButton/>
+      </TonConnectUIProvider>
   )
 }
 
