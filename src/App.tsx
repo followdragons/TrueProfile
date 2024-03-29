e7c3ad305fe45853550a5932d06c133249cb48cc
broiler99 <wer@cardify.cc>
2024-03-29 11:06:01 +0100

    moved manifest to public

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@ function App() {
   return (
       <TonConnectUIProvider
           manifestUrl="https://valeryv99.github.io/TonRockPaperScissors/tonconnect-manifest.json"
-          actionsConfiguration={{
-            twaReturnUrl: 'https://t.me/<YOUR_APP_NAME>'
-        }}
+          // actionsConfiguration={{
+            // twaReturnUrl: 'https://t.me/<YOUR_APP_NAME>'
+        // }}
       >
           <span>My App with React UI</span>
           <TonConnectButton/>
