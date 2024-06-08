0b5db71a0d315583f220e76597be9a7cce5798b8
Valery Verenich <ver.valery99@gmail.com>
2024-06-08 17:30:31 +0200

    added card

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -40,6 +40,8 @@ export const Route = createRootRoute({
           padding: 20,
           background: "rgb(229, 231, 235)",
           color: "white",
+          position: "fixed",
+          bottom: 0,
         }}
       >
         <div
